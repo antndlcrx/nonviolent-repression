182f62cb93c0ab0dc0a84dfa65ccc975225d6b77
Maksim Zubok <91369103+antndlcrx@users.noreply.github.com>
2024-08-17 17:21:18 +0100

    create non-war political viz

---
+++ code/acled/acled_non_war_polit_viz.R
@@ -0,0 +1,145 @@
+# ###
+# 
+# monthly pro-gov over time, everything else the other line 
+# 
+# vertical lines elections and war and mobilization 
+##### set up #####
+pacman::p_load(tidyverse, rio, ggplot2, lubridate)
+
+
+ACLED_DATA <- "C:/Users/murrn/GitHub/nonviolent-repression/data/acled_processed_data/acled_with_dvs_and_controls_16_07_2024_utf8byte.csv"
+
+# acled_more <- read_xlsx(ACLED_DATA)
+acled <- read_csv(ACLED_DATA)
+
+acled_subset_for_plot <- acled %>% 
+  # filter(date >= '2021-01-01') %>%
+  mutate(month = ceiling_date(date, unit = "month"),
+         pro_gov = case_when(org_indicator == "pro_kremlin" ~ "Pro Government",
+                             org_indicator == "syst_opposition" ~ "Systemic Opposition",
+                             TRUE ~ "Other"),
+         protest_type = case_when(
+                                  pred_labels == "cultural" | pred_labels == "legal" ~ "cultural/legal",
+                                  TRUE ~ pred_labels)) 
+
+# acled_subset_for_plot %>% group_by(protest_type) %>% summarise(n())
+# acled_subset_for_plot %>% group_by(pred_labels) %>% summarise(n())
+# pred_labels == "war_anti" ~ "political",
+
+# Count unique observations of 'notes' for each month
+unique_counts_type <- acled_subset_for_plot %>%
+  # filter(protest_type != "war_pro") %>% 
+  group_by(month, protest_type) %>%
+  summarise(num_protest = n_distinct(notes))
+
+
+# Create the plot
+monthly_n_plot_type <- ggplot(unique_counts_type, aes(x = month, y = num_protest, color = protest_type)) +
+  geom_line() +
+  geom_point() +
+  scale_x_date(
+    date_breaks = "1 month",
+    labels = function(x) ifelse(month(x) == 1, format(x, "%Y-%b"), format(x, "%b")),
+    expand = c(0, 0)
+  ) +
+  # scale_linetype_manual(values = c("economic" = "solid",
+  #                                  "environmental" = "dashed",
+  #                                  "cultural/legal" = "dotted",
+  #                                  "political" = "F1",
+  #                                  "social" = "dotdash")) +
+  labs(title = "Monthly Number of Unique Protests in Russia by Organiser",
+       x = "",
+       y = "Number of Unique Protests",
+       color = "Protest Type") + 
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = 6.5)) +
+  geom_vline(xintercept = as.Date("2022-02-24"), linetype = "solid", color = "black") +
+  annotate(geom = "text", x = as.Date("2022-02-24"), y = 160, label = "Invasion", vjust = -0.5, hjust = 0.5, color = "black", angle = 90, size = 3) +
+  geom_vline(xintercept = as.Date("2022-09-21"), linetype = "solid", color = "black") +
+  annotate(geom = "text", x = as.Date("2022-09-21"), y = 160, label = "Mobilisation", vjust = -0.5, hjust = 0.5, color = "black", angle = 90, size = 3) +
+  geom_vline(xintercept = as.Date("2021-01-17"), linetype = "solid", color = "black") +
+  annotate(geom = "text", x = as.Date("2021-01-17"), y = 160, label = "Navalny's retun to Moscow", vjust = -0.5, hjust = 0.5, color = "black", angle = 90, size = 3)
+
+monthly_n_plot_type
+
+
+##### What is war anto protest before 2022
+
+event <- acled %>%
+  filter(date <= '2021-01-01') %>% 
+  filter(pred_labels == "war_anti") %>% select(notes) 
+
+# "On Monday, November 26, 2018, around a dozen people took part in a anti-war protest 
+# in front the Ukrainian consulate in central Saint Peterburg following a naval clash 
+# near Kerch the day before. Even though police was present during the event, no detentions were reported."
+
+##### share of non-war political protest after invasion
+
+filtered_data <- acled %>%
+  filter(date >= '2022-02-24') %>% 
+  filter(pred_labels %in% c("political", "war_anti", "war_pro"))
+
+monthly_share <- filtered_data %>%
+  mutate(month = floor_date(as.Date(date), "month")) %>%  
+  group_by(month, pred_labels) %>%
+  summarise(count = n()) %>%
+  spread(pred_labels, count, fill = 0) %>%  
+  mutate(total = political + war_anti + war_pro,
+         political_share = political / total) %>%
+  select(month, political_share)
+
+overall_share <- filtered_data %>%
+  group_by(pred_labels) %>%
+  summarise(count = n()) %>%
+  spread(pred_labels, count, fill = 0) %>%
+  mutate(total = political + war_anti + war_pro,
+         political_share = political / total) %>%
+  select(political_share) %>%
+  pull()
+
+
+## plot
+
+# Create the plot
+monthly_n_plot_type <- ggplot(monthly_share, aes(x = month, y = political_share)) +
+  geom_line() +
+  geom_point() +
+  scale_x_date(
+    date_breaks = "1 month",
+    labels = function(x) ifelse(month(x) == 1, format(x, "%Y-%b"), format(x, "%b")),
+    expand = c(0, 0)
+  ) +
+  # scale_linetype_manual(values = c("economic" = "solid",
+  #                                  "environmental" = "dashed",
+  #                                  "cultural/legal" = "dotted",
+  #                                  "political" = "F1",
+  #                                  "social" = "dotdash")) +
+  labs(title = "Monthly Share of Political Protest Unrelated to War",
+       x = "",
+       y = "Share") + 
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = 6.5)) +
+  geom_vline(xintercept = as.Date("2022-02-24"), linetype = "solid", color = "black") +
+  annotate(geom = "text", x = as.Date("2022-02-24"), y = .60, label = "Invasion", vjust = -0.5, hjust = 0.5, color = "black", angle = 90, size = 3) +
+  geom_vline(xintercept = as.Date("2022-09-21"), linetype = "solid", color = "black") +
+  annotate(geom = "text", x = as.Date("2022-09-21"), y = .60, label = "Mobilisation", vjust = -0.5, hjust = 0.5, color = "black", angle = 90, size = 3) 
+
+monthly_n_plot_type
+
+##### Explore Political 
+
+random_sample <- acled %>%
+  filter(date >= '2022-02-24') %>% 
+  filter(pred_labels %in% c("political")) %>% 
+  sample_n(5)
+  
+
+print(random_sample$notes)
+
+
+acled %>%
+  filter(date >= '2022-02-24') %>% 
+  group_by(pred_labels) %>% 
+  summarise(n())
+
+ggsave("C:/Users/murrn/GitHub/nonviolent-repression/outputs/acled/share_political_non_war.png", plot = monthly_n_plot_type, width = 10, height = 6, dpi = 300)
